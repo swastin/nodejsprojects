5357fe6a6d3b8d10de88d4fb34314a606446cf52
swastinuser.email <swastin95.sahoo@gmail.com>
2023-05-24 17:54:49 +0530

    commited

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var env = require('dotenv').config();
 var app = express();
 var sequelize = require('./config/db_config');
-//var Users = require('./models/Users');
-//const Roles = require('./models/Roles');
+app.use(cors());
 var userRouter = require('./routes/usersRouter');
 var roleRouter = require('./routes/rolesRouter');
 var port = process.env.PORT || 8080;;
 const router = express.Router();
-//app.use('/users', userRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
